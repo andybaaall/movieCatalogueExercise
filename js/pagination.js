35b957f89a7dbb25e41a9bace24adbb4d9119bb8
Andy Ball <932400399@bun2-206-11.local>
2019-05-21 12:10:32 +1200

    ... and so is this? but this is Richard's (working) code!

--- js/pagination.js
+++ js/pagination.js
@@ -1,47 +1,152 @@
-// number of pagination pages
-// -----------------------------------------------------------------------------
+var movies = [
+  {
+    id: 1,
+    title: "Aguirre",
+    year: 1972,
+    directors: ["Werner Herzog"],
+    bio: "Aguirre, the Wrath of God (German: Aguirre, der Zorn Gottes), known in the UK as Aguirre, Wrath of God, is a 1972 West German epic historical drama film written and directed by Werner Herzog. Klaus Kinski stars in the title role. The soundtrack was composed and performed by West German progressive/Krautrock band Popol Vuh. The story follows the travels of Spanish soldier Lope de Aguirre, who leads a group of conquistadores down the Amazon River in South America in search of the legendary city of gold, El Dorado.",
+    movieLength: 95,
+    poster: "aguirre.jpg",
+    genre: ["Historical" , "Psychological" , "True Story"]
+  },
+  {
+    id: 2,
+    title: "The Great Ecstacy of Woodcarver Steiner",
+    year: 1974,
+    directors: ["Werner Herzog"],
+    bio: "The Great Ecstasy of Woodcarver Steiner (German: Die große Ekstase des Bildschnitzers Steiner) is a 1974 documentary film by German filmmaker Werner Herzog. It is about Walter Steiner, a celebrated ski jumper of his era who worked as a carpenter for his full-time occupation. Showcased is Steiner's quest for a world record in ski flying, as well as the dangers involved in the sport. Herzog has called it 'one of my most important films'.",
+    movieLength: 45,
+    poster: "theGreatEcstasy.jpg",
+    genre: ["Documentary" , "Sports" , "Woodwork"]
+  },
+  {
+    id: 3,
+    title: "The Enigma of Kaspar Hauser",
+    year: 1974,
+    directors: ["Werner Herzog" , "Werner 2" , "Werner 3"],
+    bio: "The Enigma of Kaspar Hauser (German: Jeder für sich und Gott gegen alle; lit. Every Man for Himself and God Against All) is a 1974 West German drama film written and directed by Werner Herzog and starring Bruno Schleinstein (credited as Bruno S.) and Walter Ladengast. The film closely follows the real story of foundling Kaspar Hauser, using the text of actual letters found with Hauser.",
+    movieLength: 105,
+    poster: "kasparHauser.jpg",
+    genre: ["Documentary" , "Historical"]
+  },
+  {
+    id: 4,
+    title: "How Much Wood Would a Woodchuck Chuck...",
+    year: 1976,
+    directors: ["Werner Herzog"],
+    bio: "How Much Wood Would a Woodchuck Chuck (German: Beobachtungen zu einer neuen Sprache, literally 'Observations of a New Language') is a 1976 documentary film by German director Werner Herzog, produced by Werner Herzog Filmproduktion. It is a 44-minute film documenting the World Livestock Auctioneer Championship held in New Holland, Pennsylvania. The film also contains a section about the Amish and shows Amish speaking Pennsylvania German.",
+    movieLength: 44,
+    poster: "4Shorts.jpg",
+    genre: ["Documentary" , "Language" , "Cattle"]
+  },
+  {
+    id: 5,
+    title: "Heart of Glass",
+    year: 1976,
+    directors: ["Werner Herzog"],
+    bio: "Heart of Glass (German: Herz aus Glas) is a 1976 German film directed and produced by Werner Herzog, set in 18th century Bavaria. The film was written by Herzog, based partly on a story by Herbert Achternbusch. The main character is Hias, based on the legendary Bavarian prophet Mühlhiasl.",
+    movieLength: 94,
+    poster: "heartOfGlass.jpg",
+    genre: ["Documentary" , "German Idealism" , "Aching Nostalgia"]
+  },
+  {
+    id: 6,
+    title: "Meeting Gorbachev",
+    year: 2018,
+    directors: ["Werner Herzog" , "André Singer"],
+    bio: "Meeting Gorbachev is a 2018 biographical documentary film directed by Werner Herzog and André Singer about the life of Mikhail Gorbachev, the eighth and last leader of the Soviet Union. The film features three interviews between Herzog and Gorbachev, conducted over the span of six months, and had its world premiere at the Telluride Film Festival on September 1, 2018.",
+    movieLength: 90,
+    poster: "meetingGorbachev.jpg",
+    genre: ["Documentary" , "Psychological" , "Aching Nostalgia"]
+  },
+  {
+    id: 7,
+    title: "My Son, My Son, What Have Ye Done",
+    year: 2009,
+    directors: ["Werner Herzog"],
+    bio: "My Son, My Son, What Have Ye Done? is a 2009 American-German film directed by Werner Herzog and produced by David Lynch, inspired by the story of murderer Mark Yavorsky.[2][5] Herzog describes the film as 'a horror film without the blood, chainsaws and gore, but with a strange, anonymous fear creeping up in you.' The film is an international co-production between the United States and Germany.",
+    movieLength: 91,
+    poster: "mySonMySon.jpg",
+    genre: ["True Story" , "Psychological" , "Tragedy"]
+  },
+  {
+    id: 8,
+    title: "Woyzeck",
+    year: 1979,
+    directors: ["Werner Herzog"],
+    bio: "Woyzeck is a 1979 German drama film written, produced and directed by Werner Herzog and starring Klaus Kinski and Eva Mattes. It is an adaptation of the unfinished play Woyzeck by German dramatist Georg Büchner.",
+    movieLength: 82,
+    poster: "woyzeck.jpeg",
+    genre: ["Historical" , "Tragedy" , "Adaptation"]
+  },
+  {
+    id: 9,
+    title: "Invincible",
+    year: 1979,
+    directors: ["Werner Herzog"],
+    bio: "2001 drama film written and directed by Werner Herzog. The film stars Tim Roth, Jouko Ahola, Anna Gourari, and Max Raabe. The film tells the story of a Jewish strongman in Germany. While basing his story on the real-life figure Zishe Breitbart (a.k.a. Siegmund Breitbart), Herzog uses the bare facts of Breitbart's life to weave fact and fiction (e.g., the story is set in 1932 Berlin, a full seven years after Breitbart's death in 1925) to create an allegory of human strength, knowing oneself with honesty and pride in one's heritage.",
+    movieLength: 134,
+    poster: "invincible.jpg",
+    genre: ["True Story" , "Historical"]
+  },
+  {
+    id: 10,
+    title: "Grizzly Man",
+    year: 2005,
+    directors: ["Werner Herzog"],
+    bio: "A devastating and heart-rending take on grizzly bear activists Timothy Treadwell and Amie Huguenard, who were killed in October of 2003 while living among grizzly bears in Alaska.",
+    movieLength: 104,
+    poster: "grizzlyMan.jpg",
+    genre: ["Documentary" , "Bears"]
+  },
+  {
+    id: 11,
+    title: "Cave of Forgotten Dreams",
+    year: 2005,
+    directors: ["Werner Herzog"],
+    bio: "In 1994, a group of scientists discovered a cave in Southern France perfectly preserved for over 20,000 years and containing the earliest known human paintings. Knowing the cultural significance that the Chauvet Cave holds, the French government immediately cut-off all access to it, save a few archaeologists and paleontologists. But documentary filmmaker, Werner Herzog, has been given limited access, and now we get to go inside examining beautiful artwork created by our ancient ancestors around 32,000 years ago.",
+    movieLength: 95,
+    poster: "caveOfForgottenDreams.jpg",
+    genre: ["Documentary" , "Bears"]
+  },
+  {
+    id: 12,
+    title: "Beyond the Thunderdome",
+    year: 1985,
+    directors: ["George Miller" , "George Ogilvie"],
+    bio: "Epoch-marking, genre-defining, this is definitely the best thing that has ever come out of that Godforsaken land down Under. Mel Gibson rolls around the desert in a cool car; Tina Turner attempts to gladiator-kill him with a chainsaw, which is awesome, and then there's a weird ending where he adopts a bunch of feral kids? CW: maybe a little bit racist.",
+    movieLength: 104,
+    poster: "beyondThunderdome.jpg",
+    genre: ["Guzzoline" , "Heavy Metal" , "Tina Turner" , "Ford XB GT Coupé 'V8 Interceptor'"]
+  }
+];
 
 var maxNumberOnScreen = 8;
 var numberOfPages = Math.ceil(movies.length / maxNumberOnScreen);
 
-// console.log(numberOfPages);
-
 if(numberOfPages > 1){
     var pagination = document.getElementById('paginationMovies');
     for (var i = 0; i < numberOfPages; i++) {
-        pagination.innerHTML += "<li class='page-item'><a class='page-link' href='#' data-x:'" + i + "' onclick='getContents();'>"+(i+1)+"</a></li>";
-
-        // for (var i = 1; i < numberOfPages; i++) {
-        //     pagination.innerHTML += "<li class='page-item'><a class='page-link' href='#' data-x:'" + i + "' onclick='getContents();'>"+(i+1)+"</a></li>";
-
+        pagination.innerHTML += '<li class="page-item"><a class="page-link" href="#">'+(i+1)+'</a></li>';
     }
 }
 
-// rendering the cards in increments of 8 according to which 'panel' we're on
-// -----------------------------------------------------------------------------
-
-// for onload:
-
-if (maxNumberOnScreen > movies.length){
-  console.log("fewer than 8 movies -- can't fill the screen")
-  showMovieThumbnails(0, movies.length);
+if(maxNumberOnScreen > movies.length){
+    // console.log("There are not enough movies in the database to fill the entire screen");
+    showMovieThumbnails(0, movies.length);
 } else {
-  console.log("more than 8 movies -- gotta render pagination")
-  showMovieThumbnails(0 , maxNumberOnScreen);
+    // console.log("there is more movies than the max on screen");
+    showMovieThumbnails(0, maxNumberOnScreen);
 }
 
 
-function showMovieThumbnails(start, end){      // start and end are subbing in here for (x) and (x + 7)
-  console.log(start);
-  console.log(end);
-  // instead of counting up from X, we're just going to be calling those vars 'start' and 'end'
+function showMovieThumbnails(start, end){
+    console.log(start);
+    console.log(end);
+    for (var i = start; i < end; i++) {
+        var movie = movies[i];
 
-  // now we're going to render the contents:
-  for (var i = start; i < end; i++) {
-
-    var movie = movies[i];
-
-    var movieCard = '<div class="col-12 col-sm-6 col-md-3 mb-3 text-center">';
+        var movieCard = '<div class="col-12 col-sm-6 col-md-3 mb-3 text-center">';
             movieCard += '<div class="movieThumb movieThumb2 card h-100" data-id="'+movie.id+'">';
                 movieCard += '<img src="images/posters/'+movie.poster+'" class="card-img-top" alt="">';
                 movieCard += '<div class="card-body">';
@@ -50,29 +155,6 @@ function showMovieThumbnails(start, end){      // start and end are subbing in h
             movieCard += '</div>';
         movieCard += '</div>';
 
-    document.getElementById('moviesList').innerHTML += movieCard;
-  }
+        document.getElementById('moviesList').innerHTML += movieCard;
+    }
 }
-
-// function showMovieThumbnails(start, end){
-//    console.log(start);
-//    console.log(end);
-//    for (var i = start; i < end; i++) {
-//        var movie = movies[i];
-//
-//        var movieCard = '<div class="col-12 col-sm-6 col-md-3 mb-3 text-center">';
-//            movieCard += '<div class="movieThumb movieThumb2 card h-100" data-id="'+movie.id+'">';
-//                movieCard += '<img src="images/posters/'+movie.poster+'" class="card-img-top" alt="">';
-//                movieCard += '<div class="card-body">';
-//                    movieCard += '<h5 class="card-title">'+movie.title+'</h5>';
-//                movieCard += '</div>';
-//            movieCard += '</div>';
-//        movieCard += '</div>';
-//
-//        document.getElementById('moviesList').innerHTML += movieCard;
-//    }
-// }
-
-
-
-// so this version is rendering all 12 (movies.length), and then 8 (maxNumberOnScreen)
